3256659532428a5ecd2a2916b2a7094926f2bf1d
dani-101 <48070138+dani-101@users.noreply.github.com>
2020-03-11 17:45:13 -0600

    final things

--- scripts/aboutScript.js
+++ scripts/aboutScript.js
@@ -25,15 +25,32 @@ function writeWords(context, content, X, Y){
     context.restore();  
 }
 
+function writeWordsSmall(context, content, X, Y){
+    context.save();
+    context.textAlign="center";
+    context.fillStyle = "#000000";
+    context.font = "bold 10px Arial";
+    context.fillText(content, X, Y);
+    context.restore();  
+}
+
 drawLine(context, 50, 0, 50, 450);
 drawLine(context, 50, 450, 500, 450);
+drawLine(context, 50, 350, 500, 350);
+drawLine(context, 50, 250, 500, 250);
+drawLine(context, 50, 150, 500, 150);
+drawLine(context, 50, 50, 500, 50);
 drawBar(context, 75, 75, 75, 375, "#05668D");
 drawBar(context, 175, 300, 75, 150, "#028090");
 drawBar(context, 275, 200, 75, 250, "#00A896");
 drawBar(context, 375, 150, 75, 300, "#02C39A");
-writeWords(context, "Thing1", 110, 70);
-writeWords(context, "Thing2", 210, 295);
-writeWords(context, "Thing3", 310, 195);
-writeWords(context, "Thing4", 410, 145);
-writeWords(context, "X Axis", 262.5, 495);
-writeWords(context, "Y Axis", 25, 250);
+writeWords(context, "2016", 110, 70);
+writeWords(context, "2017", 210, 295);
+writeWords(context, "2018", 310, 195);
+writeWords(context, "2019", 410, 145);
+writeWords(context, "Years", 262.5, 495);
+writeWords(context, "Sales", 25, 25);
+writeWordsSmall(context, "1 Bil", 25, 350);
+writeWordsSmall(context, "2 Bil", 25, 250);
+writeWordsSmall(context, "3 Bil", 25, 150);
+writeWordsSmall(context, "4 Bil", 25, 50);
