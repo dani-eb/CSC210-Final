8cc9e9d645c1291819b4da559e30c3632999616f
DakotaBuckner07 <61479565+DakotaBuckner07@users.noreply.github.com>
2020-03-11 13:38:43 -0600

    Put the colors in. Missing titles and that's it.

--- scripts/aboutScript.js
+++ scripts/aboutScript.js
@@ -1,8 +1,6 @@
 var canvas = document.getElementById("canvas");
 
 var context = canvas.getContext("2d");
-
-<<<<<<< HEAD
 function drawLine(context, beignX, beginY, endX, endY){
     context.save();
     context.beginPath();
@@ -29,23 +27,13 @@ function writeWords(context, content, X, Y){
 
 drawLine(context, 50, 0, 50, 450);
 drawLine(context, 50, 450, 500, 450);
-drawBar(context, 75, 75, 75, 375, "#000000");
-drawBar(context, 175, 300, 75, 150, "#000000");
-drawBar(context, 275, 200, 75, 250, "#000000");
-drawBar(context, 375, 150, 75, 300, "#000000");
+drawBar(context, 75, 75, 75, 375, "#05668D");
+drawBar(context, 175, 300, 75, 150, "#028090");
+drawBar(context, 275, 200, 75, 250, "#00A896");
+drawBar(context, 375, 150, 75, 300, "#02C39A");
 writeWords(context, "Thing1", 110, 70);
 writeWords(context, "Thing2", 210, 295);
 writeWords(context, "Thing3", 310, 195);
 writeWords(context, "Thing4", 410, 145);
 writeWords(context, "X Axis", 262.5, 495);
 writeWords(context, "Y Axis", 25, 250);
-=======
-function drawline(context, start){
-
-}
-
-
-var barChart = function(){
-
-}
->>>>>>> 7b568fa382dbbe42e20eeca8a31675f73a968d2e
